462ed9cbd8c026169f9c7e3cda045a81859e6f72
Phobetor <Phobetor@users.noreply.github.com>
2025-08-23 17:16:50 +0200

    add configuration root as constructor argument

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -34,8 +34,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fos_elastica', 'array');
+        $treeBuilder = new TreeBuilder('fos_elastica');
+        // Keep compatibility with symfony/config < 4.2
+        if (!method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->root('fos_elastica');
+        } else {
+            $rootNode = $treeBuilder->getRootNode();
+        }
 
         $this->addClientsSection($rootNode);
         $this->addIndexesSection($rootNode);
